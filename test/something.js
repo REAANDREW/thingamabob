0adf146a26cef1ae155ab2d1b0899ffde122ee98
REAANDREW <email@andrewrea.co.uk>
2014-07-06 15:38:56 +0100

    Support Message Type of CONNECT

--- test/something.js
+++ test/something.js
@@ -1,27 +1,35 @@
 var assert = require('assert');
 
 var messageTypes = {
-  RESERVED : 0
+  RESERVED: 0,
+  CONNECT: 1
 }
 
-function parseMessageType(inputBuffer){
+function parseMessageType(inputBuffer) {
   var firstByte = inputBuffer.readUInt8(0);
   var fourBitTransformation = ((0xf0 & firstByte) >> 4);
   return fourBitTransformation;
 }
 
-describe('Parsing fixed header', function(){
+describe('Parsing fixed header', function() {
 
-  describe('parses the Message Type', function(){
-  
-    it('of Reserved', function(){
-      var input = new Buffer(1);
-      input.writeUInt8(0, 0);
-      var result = parseMessageType(input);
-      assert.equal(result, messageTypes.RESERVED);
+  describe('parses the Message Type', function() {
+
+    var input;
+
+    beforeEach(function() {
+      input = new Buffer(1);
     });
 
-    
+    it('of Reserved', function() {
+      input.writeUInt8(0, 0);
+      assert.equal(parseMessageType(input), messageTypes.RESERVED);
+    });
+
+    it('of CONNECT', function() {
+      input.writeUInt8(16, 0);
+      assert.equal(parseMessageType(input), messageTypes.CONNECT);
+    });
 
   });
 
