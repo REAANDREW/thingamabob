f39a695e7bb59d2bdf4c916b3ea4e93e23239940
REAANDREW <email@andrewrea.co.uk>
2014-07-10 22:35:45 +0100

    Got the protocol name parsing in there now, using a bookmark to keep track of the ReadIndex while I go

--- test/test-parsing-message.js
+++ test/test-parsing-message.js
@@ -3,21 +3,59 @@
 var assert = require('assert');
 var thingamabob = require('../lib/thingamabob');
 
-describe('Parsing variable header', function(){
+describe('MessageParser', function() {
 
-  it.skip('parses the Protocol Name', function(){
-    var expected = 'BOBO';
-    var testProtocolName = new Buffer(expected);
-    var input = new Buffer(5 + 2 + testProtocolName.length);
-    var parser = new thingamabob.MessageParser();
-    var result = parser.parse(input);
-    var variableHeader = result.variableHeader;
-    assert.equal(variableHeader.protocolName, expected);
+  var messageParser;
+  var result;
+  var messageTypes;
+  var qualityOfService;
+  var protocolName;
+
+  beforeEach(function() {
+    messageParser = new thingamabob.MessageParser();
+    messageTypes = thingamabob.messageTypes;
+    qualityOfService = thingamabob.qualityOfService;
+    //KPAX
+    protocolName = 'PROT';
+    var input = new Buffer(8);
+    //Need to make a model for this to have a test builder
+    //as this is not clear at all what the magic numbers are
+    //unless you know the spec like the back of your hand.
+    input.writeUInt8(1 | 16 | 8 | 4 | 1, 0);
+    input.writeUInt8(127, 1);
+    input.writeUInt16BE(protocolName.length, 2);
+    input.write(protocolName, 4);
+    result = messageParser.parse(input);
+  });
+
+  it('parses fixed header', function() {
+    assert.equal(result.messageType, messageTypes.CONNECT);
+    assert.equal(result.duplicateDelivery, true);
+    assert.equal(result.qualityOfService, qualityOfService.EXACTLY_ONCE);
+    assert.equal(result.retain, true);
+    assert.equal(result.remainingLength, 127);
+  });
+
+  it('parses the variable header', function(){
+    assert.equal(result.protocolName, protocolName);
   });
 
 });
 
-describe('Parsing fixed header', function() {
+describe('ProtocolNameParser', function() {
+
+  it('Parse the protocol name', function() {
+    var buffer = new Buffer(4);
+    buffer.writeUInt16BE(0x02, 0);
+    buffer.write('AA', 2);
+    var protocolNameParser = new thingamabob.ProtocolNameParser();
+    var result = protocolNameParser.parse(buffer, new thingamabob.ReadIndex());
+    assert.equal(result, 'AA');
+  });
+
+});
+
+describe('FixedHeaderParser', function() {
 
   var parser;
   var input;
@@ -25,25 +63,29 @@ describe('Parsing fixed header', function() {
   var qualityOfService;
 
   beforeEach(function() {
-    parser = new thingamabob.MessageParser();
+    parser = new thingamabob.FixedHeaderParser();
     input = new Buffer(5);
     messageTypes = thingamabob.messageTypes;
     qualityOfService = thingamabob.qualityOfService;
   });
+
+  function parse(input) {
+    return parser.parse(input, new thingamabob.ReadIndex());
+  }
 
   describe('parses the Remaining Length', function() {
 
     it('when a single byte', function() {
       input = new Buffer(2);
       input.writeUInt8(127, 1);
-      assert.equal(parser.parse(input).remainingLength, 127);
+      assert.equal(parse(input).remainingLength, 127);
     });
 
     it('when two bytes', function() {
       input = new Buffer(3);
       input.writeUInt8(255, 1);
       input.writeUInt8(127, 2);
-      assert.equal(parser.parse(input).remainingLength, 128 * 128 - 1);
+      assert.equal(parse(input).remainingLength, 128 * 128 - 1);
     });
 
     it('when three bytes', function() {
@@ -51,54 +93,54 @@ describe('Parsing fixed header', function() {
       input.writeUInt8(255, 1);
       input.writeUInt8(255, 2);
       input.writeUInt8(127, 3);
-      assert.equal(parser.parse(input).remainingLength, 128 * 128 * 128 - 1);
+      assert.equal(parse(input).remainingLength, 128 * 128 * 128 - 1);
     });
 
-    it('when four bytes', function(){
+    it('when four bytes', function() {
       input = new Buffer(5);
       input.writeUInt8(255, 1);
       input.writeUInt8(255, 2);
       input.writeUInt8(255, 3);
       input.writeUInt8(127, 4);
-      assert.equal(parser.parse(input).remainingLength,128 * 128 * 128 * 128 - 1);
+      assert.equal(parse(input).remainingLength, 128 * 128 * 128 * 128 - 1);
     });
   });
 
   it('parses the DUP Flag', function() {
     input.writeUInt8(8, 0);
-    assert.equal(parser.parse(input).duplicateDelivery, true);
+    assert.equal(parse(input).duplicateDelivery, true);
     input.writeUInt8(0, 0);
-    assert.equal(parser.parse(input).duplicateDelivery, false);
+    assert.equal(parse(input).duplicateDelivery, false);
   });
 
   it('parses the RETAIN Flag', function() {
     input.writeUInt8(1, 0);
-    assert.equal(parser.parse(input).retain, true);
+    assert.equal(parse(input).retain, true);
     input.writeUInt8(0, 0);
-    assert.equal(parser.parse(input).retain, false);
+    assert.equal(parse(input).retain, false);
   });
 
   describe('parses the Quality of Service', function() {
 
     it('of at most once', function() {
       input.writeUInt8(0, 0);
-      assert.equal(parser.parse(input).qualityOfService, qualityOfService.AT_MOST_ONCE);
+      assert.equal(parse(input).qualityOfService, qualityOfService.AT_MOST_ONCE);
     });
 
     it('of at least once', function() {
       input.writeUInt8(2, 0);
-      assert.equal(parser.parse(input).qualityOfService, qualityOfService.AT_LEAST_ONCE);
+      assert.equal(parse(input).qualityOfService, qualityOfService.AT_LEAST_ONCE);
     });
 
 
     it('of exactly once', function() {
       input.writeUInt8(4, 0);
-      assert.equal(parser.parse(input).qualityOfService, qualityOfService.EXACTLY_ONCE);
+      assert.equal(parse(input).qualityOfService, qualityOfService.EXACTLY_ONCE);
     });
 
     it('of reserved', function() {
       input.writeUInt8(4 + 2, 0);
-      assert.equal(parser.parse(input).qualityOfService, qualityOfService.RESERVED);
+      assert.equal(parse(input).qualityOfService, qualityOfService.RESERVED);
     });
 
   });
@@ -108,79 +150,79 @@ describe('Parsing fixed header', function() {
 
     it('of Reserved', function() {
       input.writeUInt8(0, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.RESERVED);
+      assert.equal(parse(input).messageType, messageTypes.RESERVED);
       input.writeUInt8(128 + 64 + 32 + 16, 0);
-      assert.equal(parser.parse(input).messageType, 15);
+      assert.equal(parse(input).messageType, 15);
     });
 
     it('of CONNECT', function() {
       input.writeUInt8(16, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.CONNECT);
+      assert.equal(parse(input).messageType, messageTypes.CONNECT);
     });
 
     it('of CONNACK', function() {
       input.writeUInt8(32, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.CONNACK);
+      assert.equal(parse(input).messageType, messageTypes.CONNACK);
     });
 
     it('of PUBLISH', function() {
       input.writeUInt8(32 + 16, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.PUBLISH);
+      assert.equal(parse(input).messageType, messageTypes.PUBLISH);
     });
 
     it('of PUBACK', function() {
       input.writeUInt8(64, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.PUBACK);
+      assert.equal(parse(input).messageType, messageTypes.PUBACK);
     });
 
     it('of PUBREC', function() {
       input.writeUInt8(64 + 16, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.PUBREC);
+      assert.equal(parse(input).messageType, messageTypes.PUBREC);
     });
 
     it('of PUBREL', function() {
       input.writeUInt8(64 + 32, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.PUBREL);
+      assert.equal(parse(input).messageType, messageTypes.PUBREL);
     });
 
     it('of PUBCOMP', function() {
       input.writeUInt8(64 + 32 + 16, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.PUBCOMP);
+      assert.equal(parse(input).messageType, messageTypes.PUBCOMP);
     });
 
     it('of SUBSCRIBE', function() {
       input.writeUInt8(128, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.SUBSCRIBE);
+      assert.equal(parse(input).messageType, messageTypes.SUBSCRIBE);
     });
 
     it('of SUBACK', function() {
       input.writeUInt8(128 + 16, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.SUBACK);
+      assert.equal(parse(input).messageType, messageTypes.SUBACK);
     });
 
     it('of UNSUBSCRIBE', function() {
       input.writeUInt8(128 + 32, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.UNSUBSCRIBE);
+      assert.equal(parse(input).messageType, messageTypes.UNSUBSCRIBE);
     });
 
     it('of UNSUBACK', function() {
       input.writeUInt8(128 + 32 + 16, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.UNSUBACK);
+      assert.equal(parse(input).messageType, messageTypes.UNSUBACK);
     });
 
     it('of PINGREQ', function() {
       input.writeUInt8(128 + 64, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.PINGREQ);
+      assert.equal(parse(input).messageType, messageTypes.PINGREQ);
     });
 
     it('of PINGRESP', function() {
       input.writeUInt8(128 + 64 + 16, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.PINGRESP);
+      assert.equal(parse(input).messageType, messageTypes.PINGRESP);
     });
 
     it('of DISCONNECT', function() {
       input.writeUInt8(128 + 64 + 32, 0);
-      assert.equal(parser.parse(input).messageType, messageTypes.DISCONNECT);
+      assert.equal(parse(input).messageType, messageTypes.DISCONNECT);
     });
 
   });
