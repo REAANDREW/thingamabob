895aabf1f181265b15f460490f2df4adf4ef7ebc
REAANDREW <email@andrewrea.co.uk>
2014-07-13 19:53:05 +0100

    Forgot to add files

---
+++ test/test-parsing-messages.js
@@ -0,0 +1,217 @@
+'use strict';
+
+var assert = require('assert');
+var thingamabob = require('../lib/thingamabob');
+
+
+describe('Parsing', function() {
+  describe('CONNECT Messages', function() {
+
+  });
+
+  describe('CONNACK Messages', function(){
+
+    var message;
+    var parser;
+    var parsedMessage;
+
+    beforeEach(function(){
+      parser = new thingamabob.ConnAckMessageParser();
+      message = new Buffer(4);
+      message.writeUInt8(1,2);
+      parsedMessage = parser.parse(message);
+    });
+
+    it('parses the Session Present flag', function(){
+      assert.equal(parsedMessage.variableHeader.sessionPresent, true); 
+    });
+
+  });
+});
+
+
+describe('ProtocolNameParser', function() {
+
+  it('Parse the protocol name', function() {
+    var buffer = new Buffer(4);
+    buffer.writeUInt16BE(0x02, 0);
+    buffer.write('AA', 2);
+    var protocolNameParser = new thingamabob.ProtocolNameParser();
+    var result = protocolNameParser.parse(buffer, 0);
+    assert.equal(result, 'AA');
+  });
+
+});
+
+describe('FixedHeaderParser', function() {
+
+  var parser;
+  var input;
+  var messageTypes;
+  var qualityOfService;
+
+  beforeEach(function() {
+    parser = new thingamabob.FixedHeaderParser();
+    input = new Buffer(5);
+    messageTypes = thingamabob.messageTypes;
+    qualityOfService = thingamabob.qualityOfService;
+  });
+
+  function parse(input) {
+    return parser.parse(input);
+  }
+
+  describe('parses the Remaining Length', function() {
+
+    it('when a single byte', function() {
+      input = new Buffer(2);
+      input.writeUInt8(127, 1);
+      assert.equal(parse(input).remainingLength, 127);
+    });
+
+    it('when two bytes', function() {
+      input = new Buffer(3);
+      input.writeUInt8(255, 1);
+      input.writeUInt8(127, 2);
+      assert.equal(parse(input).remainingLength, 128 * 128 - 1);
+    });
+
+    it('when three bytes', function() {
+      input = new Buffer(4);
+      input.writeUInt8(255, 1);
+      input.writeUInt8(255, 2);
+      input.writeUInt8(127, 3);
+      assert.equal(parse(input).remainingLength, 128 * 128 * 128 - 1);
+    });
+
+    it('when four bytes', function() {
+      input = new Buffer(5);
+      input.writeUInt8(255, 1);
+      input.writeUInt8(255, 2);
+      input.writeUInt8(255, 3);
+      input.writeUInt8(127, 4);
+      assert.equal(parse(input).remainingLength, 128 * 128 * 128 * 128 - 1);
+    });
+  });
+
+  it('parses the DUP Flag', function() {
+    input.writeUInt8(8, 0);
+    assert.equal(parse(input).duplicateDelivery, true);
+    input.writeUInt8(0, 0);
+    assert.equal(parse(input).duplicateDelivery, false);
+  });
+
+  it('parses the RETAIN Flag', function() {
+    input.writeUInt8(1, 0);
+    assert.equal(parse(input).retain, true);
+    input.writeUInt8(0, 0);
+    assert.equal(parse(input).retain, false);
+  });
+
+  describe('parses the Quality of Service', function() {
+
+    it('of at most once', function() {
+      input.writeUInt8(0, 0);
+      assert.equal(parse(input).qualityOfService, qualityOfService.AT_MOST_ONCE);
+    });
+
+    it('of at least once', function() {
+      input.writeUInt8(2, 0);
+      assert.equal(parse(input).qualityOfService, qualityOfService.AT_LEAST_ONCE);
+    });
+
+
+    it('of exactly once', function() {
+      input.writeUInt8(4, 0);
+      assert.equal(parse(input).qualityOfService, qualityOfService.EXACTLY_ONCE);
+    });
+
+    it('of reserved', function() {
+      input.writeUInt8(4 + 2, 0);
+      assert.equal(parse(input).qualityOfService, qualityOfService.RESERVED);
+    });
+
+  });
+
+  describe('parses the Message Type', function() {
+
+
+    it('of Reserved', function() {
+      input.writeUInt8(0, 0);
+      assert.equal(parse(input).messageType, messageTypes.RESERVED);
+      input.writeUInt8(128 + 64 + 32 + 16, 0);
+      assert.equal(parse(input).messageType, 15);
+    });
+
+    it('of CONNECT', function() {
+      input.writeUInt8(16, 0);
+      assert.equal(parse(input).messageType, messageTypes.CONNECT);
+    });
+
+    it('of CONNACK', function() {
+      input.writeUInt8(32, 0);
+      assert.equal(parse(input).messageType, messageTypes.CONNACK);
+    });
+
+    it('of PUBLISH', function() {
+      input.writeUInt8(32 + 16, 0);
+      assert.equal(parse(input).messageType, messageTypes.PUBLISH);
+    });
+
+    it('of PUBACK', function() {
+      input.writeUInt8(64, 0);
+      assert.equal(parse(input).messageType, messageTypes.PUBACK);
+    });
+
+    it('of PUBREC', function() {
+      input.writeUInt8(64 + 16, 0);
+      assert.equal(parse(input).messageType, messageTypes.PUBREC);
+    });
+
+    it('of PUBREL', function() {
+      input.writeUInt8(64 + 32, 0);
+      assert.equal(parse(input).messageType, messageTypes.PUBREL);
+    });
+
+    it('of PUBCOMP', function() {
+      input.writeUInt8(64 + 32 + 16, 0);
+      assert.equal(parse(input).messageType, messageTypes.PUBCOMP);
+    });
+
+    it('of SUBSCRIBE', function() {
+      input.writeUInt8(128, 0);
+      assert.equal(parse(input).messageType, messageTypes.SUBSCRIBE);
+    });
+
+    it('of SUBACK', function() {
+      input.writeUInt8(128 + 16, 0);
+      assert.equal(parse(input).messageType, messageTypes.SUBACK);
+    });
+
+    it('of UNSUBSCRIBE', function() {
+      input.writeUInt8(128 + 32, 0);
+      assert.equal(parse(input).messageType, messageTypes.UNSUBSCRIBE);
+    });
+
+    it('of UNSUBACK', function() {
+      input.writeUInt8(128 + 32 + 16, 0);
+      assert.equal(parse(input).messageType, messageTypes.UNSUBACK);
+    });
+
+    it('of PINGREQ', function() {
+      input.writeUInt8(128 + 64, 0);
+      assert.equal(parse(input).messageType, messageTypes.PINGREQ);
+    });
+
+    it('of PINGRESP', function() {
+      input.writeUInt8(128 + 64 + 16, 0);
+      assert.equal(parse(input).messageType, messageTypes.PINGRESP);
+    });
+
+    it('of DISCONNECT', function() {
+      input.writeUInt8(128 + 64 + 32, 0);
+      assert.equal(parse(input).messageType, messageTypes.DISCONNECT);
+    });
+
+  });
+});
---
+++ test/test-sending-commands.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var assert = require('assert');
+var net = require('net');
+var thingamabob = require('../lib/thingamabob');
+var messageTypes = thingamabob.messageTypes;
+
+describe('sending', function() {
+
+  var server;
+  var port;
+  var client;
+  var fixedHeaderParser;
+
+  beforeEach(function(done) {
+    fixedHeaderParser = new thingamabob.FixedHeaderParser();
+    port = 8000;
+    server = thingamabob.createServer({
+      connectionTimeout: 20,
+      protocolVersionNumber: 4
+    });
+    server.listen('127.0.0.1', port, done);
+  });
+
+  afterEach(function(done) {
+    server.close(done);
+  });
+
+  describe('sending CONNECT command', function() {
+
+    describe('with a protocol name not equal to MQTT', function() {
+
+      it('the server closes the client connection', function(done) {
+        var protocolName = 'PROT';
+        var input = new Buffer(8);
+        input.writeUInt8(1 | 16, 0);
+        input.writeUInt8(1, 1);
+        input.writeUInt16BE(protocolName.length, 2);
+        input.write(protocolName, 4);
+        client = net.connect({
+          port: port
+        }, function() {
+          client.on('end', function() {
+            done();
+          });
+          client.write(input);
+        });
+      });
+
+    });
+
+    describe('with Clean Session set', function(){
+
+      it('returns a CONNACK message with Session Present set to false');
+      it('returns a CONNACK message with a zero return code');
+
+    });
+
+    describe('n seconds after the connection was made', function() {
+
+      it('the server closes the client connection', function(done) {
+        var n = 30;
+        client = net.connect({
+          port: port
+        }, function() {
+          var connectionEnded = false;
+          client.on('end', function() {
+            connectionEnded = true;
+          });
+          setTimeout(function() {
+            assert.ok(connectionEnded);
+            done();
+          }, n);
+        });
+      });
+
+    });
+
+    it('returns a CONNACK message  with an unacceptable protocol level', function(done) {
+
+      var protocolName = 'MQTT';
+      var input = new Buffer(8);
+      input.writeUInt8(1 | 16, 0);
+      input.writeUInt8(1, 1);
+      input.writeUInt16BE(protocolName.length, 2);
+      input.write(protocolName, 4);
+      client = net.connect({
+        port: port
+      }, function() {
+        client.on('data', function(data) {
+          var connAckMessageParser = new thingamabob.ConnAckMessageParser();
+          var message = connAckMessageParser.parse(data);
+          assert.equal(message.fixedHeader.messageType, messageTypes.CONNACK);
+          assert.equal(message.fixedHeader.remainingLength, 2);
+          client.destroy();
+          done();
+        });
+        client.write(input);
+      });
+    });
+
+    it('returns a CONNACK message', function(done) {
+      var protocolName = 'MQTT';
+      var input = new Buffer(8);
+      input.writeUInt8(1 | 16, 0);
+      input.writeUInt8(1, 1);
+      input.writeUInt16BE(protocolName.length, 2);
+      input.write(protocolName, 4);
+      client = net.connect({
+        port: port
+      }, function() {
+        client.on('data', function(data) {
+          var fixedHeaderParser = new thingamabob.FixedHeaderParser();
+          var fixedHeader = fixedHeaderParser.parse(data);
+          assert.equal(fixedHeader.messageType, messageTypes.CONNACK);
+          assert.equal(fixedHeader.remainingLength, 2);
+          client.destroy();
+          done();
+        });
+        client.write(input);
+      });
+    });
+
+  });
+
+});
