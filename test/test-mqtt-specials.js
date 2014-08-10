9c56a7ab2d640687152d70a455394f209729f936
Andrew Rea <andrew.rea@hp.com>
2014-08-10 23:34:36 +0100

    moving to a more functional style

--- test/test-mqtt-specials.js
+++ test/test-mqtt-specials.js
@@ -1,28 +1,56 @@
 var assert = require('assert');
 
-var constraints = {
-    maxMessageLength: 268435455
-};
+
+function validateRemainingLength(value) {
+    var maxMessageLength = 268435455;
+    if (value > maxMessageLength) {
+        return Error('max message length exceeded');
+    }
+    return undefined;
+}
+
+function existy(value) {
+    return value !== null && value !== undefined;
+}
+
+function dispatch() {
+    var funcs = arguments;
+
+    return function(target) {
+        for (var funcIndex in funcs) {
+            var func = funcs[funcIndex];
+            var ret = func.apply(null, [target]);
+            if (existy(ret)) {
+                return ret;
+            }
+        }
+        return undefined;
+    }
+}
 
 function encodeRemainingLength(value) {
 
-    if (value > constraints.maxMessageLength) {
-        throw new Error('max message length exceeded');
-    }
-    var buffer = new Buffer(0);
-    do {
-        var digit = value % 128;
-        value = parseInt(value / 128);
-        if (value > 0) {
-            digit = digit | 0xFF;
+    var dispatcher = dispatch(validateRemainingLength, encode);
+    
+    return dispatcher(value);
+    
+    function encode(value) {
+        var buffer = new Buffer(0);
+        do {
+            var digit = value % 128;
+            value = parseInt(value / 128);
+            if (value > 0) {
+                digit = digit | 0xFF;
+            }
+            var next = new Buffer(1);
+            next.writeUInt8(digit, 0);
+            buffer = Buffer.concat([buffer, next])
         }
-        var next = new Buffer(1);
-        next.writeUInt8(digit, 0);
-        buffer = Buffer.concat([buffer, next])
+        while (value > 0);
+        return buffer;
     }
-    while (value > 0);
-    return buffer;
 }
+
 
 function decodeRemainingLength(buffer) {
     var multiplier = 1;
@@ -38,6 +66,15 @@ function decodeRemainingLength(buffer) {
     }
     while ((digit & 128) !== 0);
     return value;
+}
+
+function remainingLengthByteCount(value) {
+    var dispatcher = dispatch(validateRemainingLength, count);
+    return dispatcher(value);
+
+    function count(value){
+        return Math.ceil(Math.log(value) / Math.log(128));
+    }
 }
 
 describe('MQTT Special Functions', function() {
@@ -69,13 +106,17 @@ describe('MQTT Special Functions', function() {
         });
 
         it('encoding a number greater than 128x128x128x127 throws an error', function() {
-            assert.throws(function() {
-                encodeRemainingLength(128 * 128 * 128 * 128);
-            }, /max message length exceeded/);
+            var error = encodeRemainingLength(128 * 128 * 128 * 128);
+            assert.equal(error.message,'max message length exceeded');
         });
     });
 
     describe('Decoding remaining length', function() {
+
+        var oneByteUpperLimit = 128 - 1;
+        var twoByteUpperLimit = 128 * 128 - 1;
+        var threeByteUpperLimit = 128 * 128 * 128 - 1;
+        var fourByteUpperLimit = 128 * 128 * 128 * 128 - 1;
 
         function expectDecoding(value, expectedValue) {
             var buffer = new Buffer(value.length);
@@ -87,29 +128,45 @@ describe('MQTT Special Functions', function() {
         }
 
         it('decodes 0x7F as one byte upper limit', function() {
-            var oneByteUpperLimit = 128 - 1;
             expectDecoding([0x7F], oneByteUpperLimit);
         });
 
         it('decodes 0xFF 0x7F as two byte upper limit', function() {
-            var twoByteUpperLimit = 128 * 128 - 1;
             expectDecoding([0xFF, 0x7F], twoByteUpperLimit);
         });
 
         it('decodes 0xFF 0xFF 0x7F as three byte upper limit', function() {
-            var threeByteUpperLimit = 128 * 128 * 128 - 1;
             expectDecoding([0xFF, 0xFF, 0x7F], threeByteUpperLimit);
         });
 
-        it('decodes 0xFF 0xFF 0xFF 0x7F as four byte upper limit', function(){
-            var fourByteUpperLimit = 128*128*128*128-1;
-            expectDecoding([0xFF,0xFF,0xFF,0x7F], fourByteUpperLimit);
+        it('decodes 0xFF 0xFF 0xFF 0x7F as four byte upper limit', function() {
+            expectDecoding([0xFF, 0xFF, 0xFF, 0x7F], fourByteUpperLimit);
         });
 
-        it('decoding a buffer greater than the four byte upper limit throws an error', function(){
-            assert.throws(function(){
-               decodeRemainingLength(new Buffer([0xFF, 0xFF, 0xFF, 0xFF]))
+        it('decoding a buffer greater than the four byte upper limit throws an error', function() {
+            assert.throws(function() {
+                decodeRemainingLength(new Buffer([0xFF, 0xFF, 0xFF, 0xFF]))
             }, /malformed remaining length/);
+        });
+
+        it('remainingLengthByteCount returns 1', function() {
+            assert.equal(1, remainingLengthByteCount(oneByteUpperLimit));
+        });
+
+        it('remainLengthByteCount returns 2', function() {
+            assert.equal(2, remainingLengthByteCount(twoByteUpperLimit));
+        });
+
+        it('remainingLengthByteCount returns 2', function() {
+            assert.equal(3, remainingLengthByteCount(threeByteUpperLimit));
+        });
+
+        it('remainingLengthByteCount returns 4', function() {
+            assert.equal(4, remainingLengthByteCount(fourByteUpperLimit));
+        });
+
+        it('remainingLengthByteCount returns error when number is greater than fourByteUpperLimit', function() {
+            assert.ok(remainingLengthByteCount(fourByteUpperLimit + 1) instanceof Error);
         });
     });
 
