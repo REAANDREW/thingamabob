51ad4570ebacdb8a95d4f8abb47e9361bdb04e5f
Andrew Rea <andrew.rea@hp.com>
2014-08-11 20:35:11 +0100

    Bug in the encoding I was using 0xFF instead of 128

--- test/test-mqtt-specials.js
+++ test/test-mqtt-specials.js
@@ -1,80 +1,5 @@
 var assert = require('assert');
-
-function validateRemainingLength(value) {
-    var maxMessageLength = 268435455;
-    if (value > maxMessageLength) {
-        return Error('max message length exceeded');
-    }
-    return undefined;
-}
-
-function existy(value) {
-    return value !== null && value !== undefined;
-}
-
-function dispatch() {
-    var funcs = arguments;
-
-    return function(target) {
-        for (var funcIndex in funcs) {
-            var func = funcs[funcIndex];
-            var ret = func.apply(null, [target]);
-            if (existy(ret)) {
-                return ret;
-            }
-        }
-        return undefined;
-    }
-}
-
-function encodeRemainingLength(value) {
-
-    var dispatcher = dispatch(validateRemainingLength, encode);
-
-    return dispatcher(value);
-
-    function encode(value) {
-        var buffer = new Buffer(0);
-        do {
-            var digit = value % 128;
-            value = parseInt(value / 128);
-            if (value > 0) {
-                digit = digit | 0xFF;
-            }
-            var next = new Buffer(1);
-            next.writeUInt8(digit, 0);
-            buffer = Buffer.concat([buffer, next])
-        }
-        while (value > 0);
-        return buffer;
-    }
-}
-
-
-function decodeRemainingLength(buffer) {
-    var multiplier = 1;
-    var value = 0;
-    var index = 0;
-    var digit;
-    do {
-        if (index === buffer.length)
-            return Error('malformed remaining length')
-        digit = buffer.readUInt8(index++);
-        value += (digit & 127) * multiplier;
-        multiplier *= 128;
-    }
-    while ((digit & 128) !== 0);
-    return value;
-}
-
-function remainingLengthByteCount(value) {
-    var dispatcher = dispatch(validateRemainingLength, count);
-    return dispatcher(value);
-
-    function count(value) {
-        return Math.ceil(Math.log(value) / Math.log(128));
-    }
-}
+var services = require('../lib/services');
 
 describe('MQTT Special Functions', function() {
 
@@ -86,7 +11,7 @@ describe('MQTT Special Functions', function() {
     describe('Encoding remaining length', function() {
 
         function expectEncoding(value, expectedValues) {
-            var result = encodeRemainingLength(value);
+            var result = services.encodeRemainingLength(value);
             assert.equal(result.length, expectedValues.length);
             for (var i = 0; i < expectedValues.length; i++) {
                 assert.equal(result.readUInt8(i), expectedValues[i]);
@@ -110,7 +35,7 @@ describe('MQTT Special Functions', function() {
         });
 
         it('encoding a number greater than four byte upper limit throws an error', function() {
-            var error = encodeRemainingLength(fourByteUpperLimit+1);
+            var error = services.encodeRemainingLength(fourByteUpperLimit + 1);
             assert.equal(error.message, 'max message length exceeded');
         });
     });
@@ -123,7 +48,7 @@ describe('MQTT Special Functions', function() {
             for (var i = 0; i < value.length; i++) {
                 buffer.writeUInt8(value[i], i);
             }
-            var result = decodeRemainingLength(buffer);
+            var result = services.decodeRemainingLength(buffer);
             assert.equal(result, expectedValue);
         }
 
@@ -144,28 +69,28 @@ describe('MQTT Special Functions', function() {
         });
 
         it('decoding a buffer greater than the four byte upper limit throws an error', function() {
-            var error = decodeRemainingLength(new Buffer([0xFF, 0xFF, 0xFF, 0xFF]))
+            var error = services.decodeRemainingLength(new Buffer([0xFF, 0xFF, 0xFF, 0xFF]))
             assert.equal(error.message, 'malformed remaining length');
         });
 
         it('remainingLengthByteCount returns 1', function() {
-            assert.equal(1, remainingLengthByteCount(oneByteUpperLimit));
+            assert.equal(1, services.remainingLengthByteCount(oneByteUpperLimit));
         });
 
         it('remainLengthByteCount returns 2', function() {
-            assert.equal(2, remainingLengthByteCount(twoByteUpperLimit));
+            assert.equal(2, services.remainingLengthByteCount(twoByteUpperLimit));
         });
 
         it('remainingLengthByteCount returns 2', function() {
-            assert.equal(3, remainingLengthByteCount(threeByteUpperLimit));
+            assert.equal(3, services.remainingLengthByteCount(threeByteUpperLimit));
         });
 
         it('remainingLengthByteCount returns 4', function() {
-            assert.equal(4, remainingLengthByteCount(fourByteUpperLimit));
+            assert.equal(4, services.remainingLengthByteCount(fourByteUpperLimit));
         });
 
         it('remainingLengthByteCount returns error when number is greater than fourByteUpperLimit', function() {
-            assert.ok(remainingLengthByteCount(fourByteUpperLimit + 1) instanceof Error);
+            assert.ok(services.remainingLengthByteCount(fourByteUpperLimit + 1) instanceof Error);
         });
     });
 
